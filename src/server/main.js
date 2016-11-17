b17a9c406d92ed9c5fb1b03b34dcf4db85acabf7
Daryl Roberts <daryl.roberts@gmail.com>
2016-11-16 16:48:01 -0800

    update server port to be more dynamic

--- src/server/main.js
+++ src/server/main.js
@@ -8,6 +8,7 @@ import convert from 'koa-convert'
 import userAgent from 'koa-useragent'
 import kcors from 'kcors'
 import path from 'path'
+import config from 'config'
 
 import cache from './cache'
 import router from './router'
@@ -27,11 +28,14 @@ const assets = serve(path.resolve(__dirname + '/../../public'))
 app.use(mount('/public', assets))
 app.use(router.routes())
 
-app.listen(8181, 'localhost', function (err) {
+const { port } = config
+console.log(config)
+
+app.listen(port, 'localhost', function (err) {
   if (err) {
     console.log(err)
     return
   }
 
-  console.log('Node server listening on http://localhost:8181/')
+  console.log(`Node server listening on http://localhost:${port}`)
 })
